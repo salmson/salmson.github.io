0cc3ddf68596a4909e38bd652798baf1857c10e2
salmson <131785423+salmson@users.noreply.github.com>
2024-12-28 14:33:32 +0900

    Create high_way.js

---
+++ high_way.js
@@ -0,0 +1,77 @@
+let jc =0
+let longs =0;
+
+function high_way_math(number){
+  
+}
+
+function load_jc(hw_num,distance,muki){
+  var next_jc ="";
+  var jc_name ="null";
+  var befor_jc ="";
+
+  switch(hw_num){
+    case 0://道央
+      break;
+    case 1://東北
+      break;
+    case 2://常磐
+      break;
+    case 3://東名
+      break;
+    case 4://中央
+      break;
+    case 5://北陸
+      break;
+    case 6://名神
+      break;
+    case 7://中国
+      break;
+    case 8://九州
+      break;
+    case 9://沖縄
+      break;
+    case 10://SA-PA
+      break;
+    default:
+      break;
+  }
+  return [befor_jc , jc_name , next_jc];
+}
+
+let highway = [
+  ["道央自動車道",""],
+  ["東北自動車",""],
+  ["常磐自動車道",""],
+  ["東名高速道路",""],
+  ["中央自動車",""],
+  ["北陸自動車",""],
+  ["名神高速道路",""],
+  ["中国自動車道",""],
+  ["九州自動車道",""],
+  ["沖縄自動車道",""]
+];
+
+//346.2
+//九州自動車道
+let ksE =[
+  ["鹿児島IC",0],
+  ["桜島SA/SIC",22.6],
+  ["加治木JCT",28.7],
+  ["えびのJCT",69.8],
+  ["人善球磨SIC",90.4],
+  ["八代JCT",131.6],
+  ["宇城氷川SIC",142.2],
+  ["城南BS/SIC",157.2],
+  ["嘉島JCT",165.5],
+  ["北熊本SIC",184.2],
+  ["鳥栖JCT",250.1],
+  ["太宰府IC",266.1],
+  ["須恵PA/SIC",272.8],
+  ["福岡IC",277.9],
+  ["宮田SIC",304.1],
+  ["八幡IC",314.8],
+  ["北九州JCT",329.7],
+  ["小倉東IC",332.8],
+  ["門司IC",346.2]
+];
